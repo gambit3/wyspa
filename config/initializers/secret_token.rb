e8b256f9f07d7c2e5494a0cb20c01e1fc81afd94
Artur Marczyk <artur@Arturs-MacBook-Air.local>
2011-10-24 12:17:19 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wyspa::Application.config.secret_token = '3d8793a14a13d6651a90a53c5aea2eeec1dd43d231cfd50272b023ed8fe82adcb48808a9a5bfe192f789118b974ef6e9bcc5039a166899a9c26c0e52b50a1440'
